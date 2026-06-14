2e26ce0dd0a165d46e13051341b9b0ea01b585e3
Dzzz83 <phattuan43@gmail.com>
2026-06-14 13:58:08 +0700

    fix bugs

--- demo/run-servers.bat
+++ demo/run-servers.bat
@@ -1,10 +1,19 @@
 @echo off
-cd /d "%~dp0..\server"
-start "Server-8080" java -jar target\server-1.0-SNAPSHOT.jar --server.port=8080 --server.address=0.0.0.0
+cd /d "%~dp0.."
+
+echo Starting Server-8080...
+start "Server-8080" java -jar server\target\server-1.0-SNAPSHOT.jar --server.port=8080 --server.address=0.0.0.0
 timeout /t 2 /nobreak >nul
-start "Server-8081" java -jar target\server-1.0-SNAPSHOT.jar --server.port=8081 --server.address=0.0.0.0
+
+echo Starting Server-8081...
+start "Server-8081" java -jar server\target\server-1.0-SNAPSHOT.jar --server.port=8081 --server.address=0.0.0.0
 timeout /t 2 /nobreak >nul
-start "Server-8082" java -jar target\server-1.0-SNAPSHOT.jar --server.port=8082 --server.address=0.0.0.0
+
+echo Starting Server-8082...
+start "Server-8082" java -jar server\target\server-1.0-SNAPSHOT.jar --server.port=8082 --server.address=0.0.0.0
 timeout /t 2 /nobreak >nul
-start "Server-8083" java -jar target\server-1.0-SNAPSHOT.jar --server.port=8083 --server.address=0.0.0.0
+
+echo Starting Server-8083...
+start "Server-8083" java -jar server\target\server-1.0-SNAPSHOT.jar --server.port=8083 --server.address=0.0.0.0
+
 echo All server nodes started.
